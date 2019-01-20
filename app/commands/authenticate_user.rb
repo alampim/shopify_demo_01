487bf0142108fa9553cf05cf558c2a983d685815
Edward Crowder <edward@crowdere.com>
2019-01-20 18:08:49 -0500

    Updated secret key base to rails v5.2 standard

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -1,26 +1,27 @@
 #The command takes the user's e-mail and password then 
 #returns the user, if the credentials match.
-Class AuthenticateUser
+
+class AuthenticateUser
     prepend SimpleCommand
-
+  
     def initialize(email, password)
-        @email = email
-        @password = password
+      @email = email
+      @password = password
     end
-
+  
     def call
-        JsonWebToken.encode(user_id: user.id) if user
+      JsonWebToken.encode(user_id: user.id) if user
     end
-
-private
-
+  
+    private
+  
     attr_accessor :email, :password
-
+  
     def user
-        user = User.find_by_email(email)
-        return user if user && user.authenticate(password)
-
-    errors.add :user_authentication, 'invalid credentials'
-        nil
+      user = User.find_by_email(email)
+      return user if user && user.authenticate(password)
+  
+      errors.add :user_authentication, 'invalid credentials'
+      nil
     end
-end
+  end
